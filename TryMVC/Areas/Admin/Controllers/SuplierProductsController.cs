5aba6a80932f80dca8a11c9cfd683fb23ee5567a
alanwugithub <alan.wu.github@gmail.com>
2018-04-20 00:59:57 +0800

    新增頁面 - SuplierProducts
    [Display all products of each suplier]
    @ 於 Area/Admin/Views/web.config, 將 model / viewModel 的 namespace 加入.
    @ 新增 ViewModel - SuplierProductsViewModels.cs
    @ 新增 Controller - SuplierProductsController.cs

---
+++ TryMVC/Areas/Admin/Controllers/SuplierProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TryMVC.Models;
+using TryMVC.Areas.Admin.ViewModels;
+using System.Net;
+
+namespace TryMVC.Areas.Admin.Controllers
+{
+    public class SuplierProductsController : Controller
+    {
+        private NorthWindEntities db = new NorthWindEntities();
+
+        // GET: Admin/SuplierProducts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var supplier = db.Suppliers.Find(id);
+            var productCollection = db.Products.Where(x => x.CategoryID == id).ToList();
+
+            var viewModel = new SuplierProductsViewModel();
+            viewModel.Supplier = supplier;
+            viewModel.ProductCollection = productCollection;
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ TryMVC/Areas/Admin/ViewModels/SuplierProductsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TryMVC.Models;
+
+namespace TryMVC.Areas.Admin.ViewModels
+{
+    public class SuplierProductsViewModel
+    {
+        public Suppliers Supplier { get; set; }
+
+        public IEnumerable<Products> ProductCollection { get; set; }
+    }
+}
